23733db5b55c0ec46b8dd0bdbada039176c1ab1e
Bubo Howler <49031316+rarestowl@users.noreply.github.com>
2019-07-03 19:49:51 +0530

    Create sub_sequence.cpp

---
+++ sub_sequence.cpp
@@ -0,0 +1,20 @@
+Increasing/Decreasing/Bitonic Sequences of Numbers
+Basic:
+34. Longest Increasing Subsequence (https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/)
+37. Maximum sum Bi-tonic Sub-sequence (https://www.geeksforgeeks.org/maximum-sum-bi-tonic-sub-sequence/)
+38. Maximum Sum Increasing Subsequence (https://www.geeksforgeeks.org/maximum-sum-increasing-subsequence-dp-14/)
+39. Maximum product of an increasing subsequence (https://www.geeksforgeeks.org/maximum-product-increasing-subsequence/)
+
+Intermediate:
+27. Longest Bitonic Subsequence (https://www.geeksforgeeks.org/longest-bitonic-subsequence-dp-15/)
+28. Printing Longest Bitonic Subsequence (https://www.geeksforgeeks.org/printing-longest-bitonic-subsequence/)
+
+Subsequence of characters(strings)
+Basic:
+32. Longest Common Subsequence (https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/)
+33. Longest Repeated Subsequence (https://www.geeksforgeeks.org/longest-repeated-subsequence/)
+35. A Space Optimized Solution of LCS (https://www.geeksforgeeks.org/space-optimized-solution-lcs/)
+36. LCS (Longest Common Subsequence) of three strings (https://www.geeksforgeeks.org/lcs-longest-common-subsequence-three-strings/)
+
+
+
