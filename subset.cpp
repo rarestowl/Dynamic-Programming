b8f5c139b1b84bfc0dde8271aed63bf54ce72625
Bubo Howler <49031316+rarestowl@users.noreply.github.com>
2019-07-04 19:55:55 +0530

    Create subset.cpp

---
+++ subset.cpp
@@ -0,0 +1,24 @@
+0/1 Knapsack Problem:
+Intermediate:
+10. 0-1 Knapsack Problem (https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/)
+11. Printing Items in 0/1 Knapsack (https://www.geeksforgeeks.org/printing-items-01-knapsack/)
+
+Cutting Rod:
+Basic:
+9.  Coin Change(https://www.geeksforgeeks.org/coin-change-dp-7/)
+//Pick all coins one by one and update the table[] values after the index >= the value of the picked coin 
+18. Cutting a Rod (https://www.geeksforgeeks.org/cutting-a-rod-dp-13/)
+Intermediate:
+12. Unbounded Knapsack (Repetition of items allowed) (https://www.geeksforgeeks.org/unbounded-knapsack-repetition-items-allowed/)
+
+Subset Sum:-
+Basic:
+11. Subset Sum Problem (https://www.geeksforgeeks.org/subset-sum-problem-dp-25/)
+12. Subset Sum Problem in O(sum) space (https://www.geeksforgeeks.org/subset-sum-problem-osum-space/)
+13. Subset with sum divisible by m (https://www.geeksforgeeks.org/subset-sum-divisible-m/)
+
+Intermediate:
+22. Partition problem (https://www.geeksforgeeks.org/partition-problem-dp-18/)
+23. Print equal sum sets of array (Partition problem) | Set 1 (https://www.geeksforgeeks.org/print-equal-sum-sets-array-partition-problem/)
+24. Print equal sum sets of array (Partition Problem) | Set 2 (https://www.geeksforgeeks.org/print-equal-sum-sets-array-partition-problem-set-2/)
+ 
