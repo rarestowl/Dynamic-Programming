94b418f8468d111a8ba8e8c0cfccca3dd753eb0b
Bubo Howler <49031316+rarestowl@users.noreply.github.com>
2019-07-13 11:49:32 +0530

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -1,6 +1,9 @@
 questions:
 1. search in sorted and rotated array
-2. search in bitonic array
+2. search in bitonic array (find pivot point)
 3. find median in two sorted arrays of different sizes
 4. kth largest element from array(min heap) / kth
 5. ceiling/ floor in sorted/unsorted array
+6. find k most frequent words from file
+7. find peak element
+8. Minimum element in a sorted and rotated array 
