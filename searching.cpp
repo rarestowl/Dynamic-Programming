1de04acd2a5001768842c4df213528018b123596
Bubo Howler <49031316+rarestowl@users.noreply.github.com>
2019-07-13 09:14:31 +0530

    Create searching.cpp

---
+++ searching.cpp
@@ -0,0 +1,6 @@
+questions:
+1. search in sorted and rotated array
+2. search in bitonic array
+3. find median in two sorted arrays of different sizes
+4. kth largest element from array(min heap) / kth
+5. ceiling/ floor in sorted/unsorted array
